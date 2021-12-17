e84c1af2fe83f99878fa974ebf194353fa2e9a86
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-12-17 17:27:22 +0200

    MetaMask network switch logic implemented

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/aurora-logo.png" @click="checkUserChainId" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    
+    async checkUserChainId() {
+      const AURORA_MAINNET_CHAIN_ID = 1313161554;
+
+      if (!window.ethereum || window.ethereum.chainId === null) {
+        setTimeout(checkUserChainId, 500);
+
+        return;
+      }
+
+      if (Number.parseInt(window.ethereum.chainId) !== AURORA_MAINNET_CHAIN_ID) {
+        try {
+          await window.ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: `0x${AURORA_MAINNET_CHAIN_ID.toString(16)}` }],
+          });
+        } catch (switchError) {
+          if (switchError.code === 4902) { // This error code indicates that the chain has not been added to MetaMask
+            try {
+              await window.ethereum.request({
+                method: 'wallet_addEthereumChain',
+                params: [
+                  {
+                    chainId: `0x${AURORA_MAINNET_CHAIN_ID.toString(16)}`,
+                    chainName: "Aurora Mainnet",
+                    nativeCurrency: {
+                      name: 'Aurora ETH',
+                      symbol: 'ETH',
+                      decimals: 18
+                    },
+                    rpcUrls: ["https://mainnet.aurora.dev"],
+                    blockExplorerUrls: ['https://explorer.mainnet.aurora.dev/']
+                  }
+                ],
+              });
+            } catch (addError) {
+              console.log(addError);
+            }
+          }
+        }
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+div#app {
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+img {
+  width: 200px;
+  margin: 0 auto;
+  cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,64 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { createClient } from '@remixproject/plugin-iframe';
+
+const AURORA_MAINNET_CHAIN_ID = 1313161554;
+
+const client = createClient();
+client.onload(async () => {
+  await checkUserChainId();
+
+  //const data = await client.call('fileManager', 'readFile', 'contracts/AuroraBooking.sol');
+
+  //console.log(data);
+
+  client.on('solidity', 'compilationFinished', async (fileName, result) => {
+    //let result = await client.call('solidity', 'getCompilationResult', fileName);
+    //TODO: implement logic
+  });
+})
+
+let checkUserChainId = async () => {
+  if (!window.ethereum || window.ethereum.chainId === null) {
+    setTimeout(checkUserChainId, 500);
+
+    return;
+  }
+
+  if (Number.parseInt(window.ethereum.chainId) !== AURORA_MAINNET_CHAIN_ID) {
+    try {
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: `0x${AURORA_MAINNET_CHAIN_ID.toString(16)}` }],
+      });
+    } catch (switchError) {
+      if (switchError.code === 4902) { // This error code indicates that the chain has not been added to MetaMask
+        try {
+          await window.ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: `0x${AURORA_MAINNET_CHAIN_ID.toString(16)}`,
+                chainName: "Aurora Mainnet",
+                nativeCurrency: {
+                  name: 'Aurora ETH',
+                  symbol: 'ETH',
+                  decimals: 18
+                },
+                rpcUrls: ["https://mainnet.aurora.dev"],
+                blockExplorerUrls: ['https://explorer.mainnet.aurora.dev/']
+              }
+            ],
+          });
+        } catch (addError) {
+          console.log(addError);
+        }
+      }
+    }
+  }
+}
+
 Vue.config.productionTip = false
 
 new Vue({
